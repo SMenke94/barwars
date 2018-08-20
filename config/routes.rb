5a5902f75dbf5bd7ca80eb136d579c2e3bdb69ce
Amanda Schjørmann <amandaschjoermann@gmail.com>
2018-08-20 16:41:19 +0200

    basic layout and flow through the pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :deals, only: [:index, :show]
-  resources :dashboard, only: [:show]
+  get "dashboard", to: "dashboard#show", as: "dashboard"
   get 'loading', to: 'pages#loading'
   get 'weekly-deals', to: 'pages#weekly_deals'
 end
