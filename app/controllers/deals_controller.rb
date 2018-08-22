abe2dc66dcb1d66460ba345d1ab5008dd07c4ccd
Charles Sumner <cws1717@gmail.com>
2018-08-22 14:16:00 +0200

    category filters work, using AJAX, no submit button

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,6 +1,21 @@
 class DealsController < ApplicationController
   def index
     @deals = Deal.all
+    if params[:type]
+      @deals = Deal.joins(:type).where(types: {name: params[:type]})
+      respond_to do |format|
+        format.html
+        format.js  # <-- idem
+      end
+        # Deal.joins(:type).where('types.name = ?', params[:type][0])
+        # @deals = @deals.where(type: [params[:type]])
+      # elsif params[:q].present?
+      #   sql_query = "name ILIKE :q OR description ILIKE :q OR category ILIKE :q"
+      #   @gears = @gears.where(sql_query, q: "%#{params[:q]}%")
+      end
+
+    @types = Type.all
+
   end
 
   def show
