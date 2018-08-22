83d6933dac4941528730aa2d825768be28387521
Amanda Schjørmann <amandaschjoermann@gmail.com>
2018-08-22 09:57:07 +0200

    added some restrictions to the slider and updated the seeds for today

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,10 +1,11 @@
 class DealsController < ApplicationController
   def index
     @deals = Bar.near(params[:q], params[:distance]).map(&:deals).flatten.select!(&:valid_now?)
-
-    flash[:notice] = "No result found" if @deals.nil?
     if params[:q]
       @current_location = Geocoder.search(params[:q]).first.coordinates
+    end
+    if @deals.nil?
+      @deals = Deal.all
     end
   end
 
