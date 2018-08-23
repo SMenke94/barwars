b2f6c4d06fb94004a6a1f8e1971e746230307f69
Amanda Schjørmann <amandaschjoermann@gmail.com>
2018-08-23 14:57:03 +0200

    styled the filterbox and added some checkboxes

--- app/javascript/current_location.js
+++
@@ -1,7 +0,0 @@
-navigator.geolocation.getCurrentPosition(function(position) {
-  const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(lat);
-  console.log(lng);
-  raise
-});
