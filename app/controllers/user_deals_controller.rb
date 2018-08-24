e5630476a6d1cb9d7ddf63bdfc21fc9480dbdfd5
Amanda Schjørmann <amandaschjoermann@gmail.com>
2018-08-24 13:02:18 +0200

    now you can save a deal and the icon change depending on its status

---
+++ app/controllers/user_deals_controller.rb
@@ -0,0 +1,28 @@
+class UserDealsController < ApplicationController
+  before_action :set_deal
+  before_action :authenticate_user!
+
+  def create
+    @user_deal = UserDeal.new
+    @user_deal.user = current_user
+    @user_deal.deal = @deal
+    if @user_deal.save!
+      redirect_to deals_path
+    else
+      redirect_to deals_path, alert: "Deal not saved"
+    end
+  end
+
+  def update
+    @user_deal = UserDeal.find_by(user: current_user, deal: @deal)
+    @user_deal.destroy
+
+    redirect_to deals_path
+  end
+
+  private
+
+  def set_deal
+    @deal = Deal.find(params[:deal_id])
+  end
+end
