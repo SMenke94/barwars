0fad7c0b3bb94fa23ab1dabff8cbb241928b7adf
Amanda Schjørmann <amandaschjoermann@gmail.com>
2018-08-29 16:15:13 +0200

    ajax fix

--- app/controllers/user_deals_controller.rb
+++ app/controllers/user_deals_controller.rb
@@ -6,12 +6,7 @@ class UserDealsController < ApplicationController
     @user_deal = UserDeal.new
     @user_deal.user = current_user
     @user_deal.deal = @deal
-    if @user_deal.save!
-      respond_to do |format|
-        format.html { redirect_to deals_path}
-        format.js
-      end
-    else
+    if !@user_deal.save!
       redirect_to deals_path, alert: "Deal not saved"
     end
   end
@@ -19,8 +14,6 @@ class UserDealsController < ApplicationController
   def update
     @user_deal = UserDeal.find_by(user: current_user, deal: @deal)
     @user_deal.destroy
-
-    redirect_to deals_path
   end
 
   private
