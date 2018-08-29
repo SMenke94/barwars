c8341edad38bde317a64b3dc9b9cce352151c11e
Nicolas Feer <atmosfeer@gmail.com>
2018-08-29 14:58:56 +0200

    first try fb button

---
+++ app/javascript/packs/fb-share.js
@@ -0,0 +1,9 @@
+// const button = document.getElementById("fb-share-button")
+
+// button.addEventListener("click", (event) => {
+//   FB.ui({
+//     method: 'share',
+//     href: event.currentTarget.dataset.url,
+//   }, function(response){});
+// });
+
