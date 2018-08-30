f44490cbaac78d3068cb1ed1b69661ed7a14e65f
Amanda Schjørmann <amandaschjoermann@gmail.com>
2018-08-30 21:55:59 +0200

    nick animation

--- app/javascript/packs/filter-button.js
+++ app/javascript/packs/filter-button.js
@@ -26,3 +26,26 @@ const mulletWrapper = document.getElementById('mullet-wrapper');
 logoButton.addEventListener('click', function() {
   mulletWrapper.classList.toggle('is-visible');
 });
+
+
+const movingBeer1 = document.getElementById('moving-beer1');
+const moveBeer1 = document.getElementById('red-beer1');
+
+
+movingBeer1.addEventListener('click', function() {
+  moveBeer1.classList.add('movebeer1');
+  setTimeout(function() {
+  $('#red-beer1').removeClass('movebeer1');
+}, 500);
+});
+
+const movingBeer2 = document.getElementById('moving-beer2');
+const moveBeer2 = document.getElementById('red-beer2');
+
+
+movingBeer2.addEventListener('click', function() {
+  moveBeer2.classList.add('movebeer2');
+  setTimeout(function() {
+  $('#red-beer2').removeClass('movebeer2');
+}, 500);
+});
